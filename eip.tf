b10ce50c5d22cb8a3fdd8c565bbc147fa7281c34
hugo.romero <hugo.romero@kushkipagos.com>
2024-05-26 10:18:30 -0500

    nueva ec2

--- eip.tf
+++ eip.tf
@@ -1,7 +1,7 @@
 resource "aws_eip" "elastic_ip_ejercicio" {
     domain = "vpc"
-    instance = aws_instance.server_ejercicio.id
-    depends_on = [ aws_internet_gateway.int_gw_vpc_actividad ]
+    instance = aws_instance.server_wordpress.id
+    depends_on = [ aws_internet_gateway.int_gw_vpc_proy ]
     associate_with_private_ip = "172.16.10.4"
     tags = {
     Name = "eip_ejercicio"
