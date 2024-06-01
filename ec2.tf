5550808bb3c0189f867d7c4d3e418dae224202ee
hugo.romero <hugo.romero@kushkipagos.com>
2024-05-31 20:08:28 -0500

    infrastructura completa funcional

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "server_wordpress" {
-  ami = "ami-058bd2d568351da34"
+  ami = "ami-0e001c9271cf7f3b9"
   instance_type = "t2.micro"
   key_name = "roberKey"
   subnet_id = aws_subnet.subnet_vpc_proyecto.id
